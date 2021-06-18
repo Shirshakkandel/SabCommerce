541dc6dd59e5cdc25e9f0180e344fe7e57ba0c88
shirshakkandel <shirshakkandel@gmail.com>
2021-06-18 13:21:58 +0545

    file for deployment"

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from 'express'
 import dotenv from 'dotenv'
 import colors from 'colors'
 import morgan from 'morgan'
+import path from 'path'
 
 import connectDB from './config/db.js'
 import userRoutes from './routes/userRoutes.js'
@@ -16,12 +17,22 @@ if (process.env.NODE_ENV === "development") {
      app.use(morgan('dev'))
 }
 
+const __dirname = path.resolve()
+if (process.env.NODE_ENV === 'production') {
+     app.use(express.static(path.join(__dirname, '/frontend/build')))
+     app.get('*', (req, res) => {
+          res.sendFile(path.resolve(__dirname,'frontend','build', 'index.html'))
+     })
+} else {
+     app.use('/', (req, res) => { res.send("Api is working") })
+}
+
 app.use(express.json())
 
 app.use('/api/users', userRoutes)
 
 
-app.use('/', (req, res) => { res.send("Api is working") })
+
 app.use(notFound)
 app.use(errorHandler)
 
