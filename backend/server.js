37e4ca1258d0b7f64c5c4adcf6297c1c6338da76
shirshakkandel <shirshakkandel@gmail.com>
2021-06-18 13:36:51 +0545

    PORT added"

--- backend/server.js
+++ backend/server.js
@@ -36,5 +36,7 @@ app.use('/api/users', userRoutes)
 app.use(notFound)
 app.use(errorHandler)
 
-app.listen(5000,console.log(`Server is running in 5000`))
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT,console.log(`Server is running in 5000`))
 
